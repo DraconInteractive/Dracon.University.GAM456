722c45bde5c7215cecb5891f76dbe0f74bf8637a
RarCeth <rarceth@gmail.com>
2018-05-25 19:43:46 +0800

    asdf

---
+++ Assets/TiledWorld/Scripts/Tile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+	public enum TileType
+	{
+		Free,
+		Obstructed
+	}
+
+	public TileType Type;
+}
